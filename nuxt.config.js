89f4bd63c732d927d1bd5211aa92410629c7ec2f
crabstertechnology <crabstertechnology@gmail.com>
2025-04-11 15:11:38 +0530

    trying to upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
-  css: [`~/style.css`],
-  plugins: [`~/plugins/lottie-vue-player.client.js`],
+  target: 'static', // Required for static site generation compatible with Netlify
+  css: ['~/style.css'], // Global CSS inclusion
+  plugins: ['~/plugins/lottie-vue-player.client.js'], // Client-side plugin for Lottie animations
 }
